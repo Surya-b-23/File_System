05cbd1c0335ed44822f6a3fc88e4e06bb3289d52
Surya-b-23 <84794242+Surya-b-23@users.noreply.github.com>
2022-03-15 10:34:48 +0530

    FS

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
-fs.readFile('info.json', function(err, data) {
-   console.log(data) 
-  });
+fs.readFile("info.json",function(err, data){
+    var content = data.toString();
+    convert_json(content);
+})
+var Age = [21,19]
+function convert_json(content){
+    var convert_json = JSON.parse(content);
+    for(var i =0; i < Object.keys(convert_json).length; i++){
+        convert_json[i].Age = Age[i]
+    }
+    var write = JSON.stringify(convert_json)
+    fs.writeFile("update.json",write,function(err){
+        if (err) console.log(err)
+    })
+}
